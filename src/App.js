4dbcd11ecc3ecc8cb4a55b1f4cc5888b2063746c
Rohit Kucheria <rohit.kucheria@rohitkr303.successivetech.com>
2023-08-02 17:59:06 +0530

    import export functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from "react";
-import FunctionalComponent from "./components/FunctionalComponent";
+
+// if we use one by one import functionality then that is too Complax;
+// import Project, {allocation,TestComponent,functional,testfunctions} from "./components/TestComponent";
+
+// So that we use this process
+import * as compObj from './components/TestComponent';
+
 function App(){
     return (
       <div className="App">
-        <FunctionalComponent/>
+        {/* <ol>
+          <li>{Project}</li>
+          <li>{allocation}</li>
+          <li>{functional()}</li>
+          <li>{testfunctions()}</li>
+          <li><TestComponent /></li>
+        </ol> */}
+
+        <ol>
+          <li>{compObj.default}</li>
+          <li>{compObj.allocation}</li>
+          <li>{compObj.functional()}</li>
+          <li>{compObj.testfunctions()}</li>
+          {/* <li><TestComponent /></li> */}
+        </ol>
+
+
       </div>
     );
   }
---
+++ src/components/TestComponent.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const project = 'LMS';
+const allocation = '8 hours';
+
+function TestComponent(){
+    return <>
+    <h3>Hello Team</h3>
+    </>
+}
+function functional(){
+    let duration = '1 month';
+    return duration;
+} 
+
+function testfunctions(){
+    let type = 'Internal';
+    return type;
+}
+
+export default project;
+export { allocation ,TestComponent, functional, testfunctions};
