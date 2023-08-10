ba47fc78a2a2e569de91830e68e3c955da08dd43
Rohit Kucheria <rohit.kucheria@rohitkr303.successivetech.com>
2023-08-10 13:39:14 +0530

    onclick state change

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import './App.css';
-// import Greet from './components/functionalComponent';
-import { Greet } from './components/functionalComponent';
-import ClassComponent from './components/classComponent';
-import { Component } from 'react';
+import { useState } from 'react';
+import './index.css';
 
+// ******************************************* Array Destracturing **********************
 
-class App extends Component{
-  render() {
-    const user = 'Rohit';
+// const arr = ['Rohit','Kucheria','HRMS'];
+
+// const [first_name ,last_name,Project] = arr;   // Array Destracturing
+// console.log(Project,last_name,first_name);
+
+//************************************************************************************** */
+
+const App = () => {
+    const [count,ChangeStateCount] = useState(0);
+    const IncN = () => {
+      ChangeStateCount(count+1);
+    }
+    
     return (
       <div className="App">
-        <Greet></Greet>
-        <ClassComponent />
-        {user}
+        <h1 className='heading'> Welcome to React Application</h1>
+
+        <h1> {count}</h1><br/>
+        <button className='button1' onClick={IncN}> Click Me </button>
       </div>
     );
-  }
   }
   
 
