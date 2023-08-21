70054d82c4ac42d962f3cd71351cabaca1341249
Rohit Kucheria <rohit.kucheria@rohitkr303.successivetech.com>
2023-08-21 12:30:00 +0530

    react form submit done

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,28 +1,83 @@
 import React, { useState } from "react";
 import "./index.css";
 function Form() {
-  const [name, chnageName] = useState("");
-  const [full_name, setFullName] = useState("");
+  const [name, chnageName] = useState({
+    fname : '',
+    lname : '',
+    email : '',
+  });
+  
   const setValue = (event) => {
-    chnageName(event.target.value);
+    const value = event.target.value;
+    const name = event.target.name;
+
+    chnageName((preValue) => {
+      if(name === 'fname'){
+        return {
+          fname: value,
+          lname: preValue.lname,
+          email: preValue.email
+        }
+      }
+      else if(name === 'lname'){
+        return {
+          fname: preValue.fname,
+          lname: value,
+          email: preValue.email
+        }
+      }
+      else if(name === 'email'){
+        return {
+          fname: preValue.fname,
+          lname: preValue.lname,
+          email: value
+        }
+      }
+    });
   };
+
   const formSubmit = (event) => {
     event.preventDefault();
-    setFullName(name);
-    chnageName("");
+    alert('Form Submited');
+    chnageName({
+      fname: '',
+      lname: '',
+      email: ''
+    });
   };
+
+  
   return (
     <>
-      <div style={{ margin: 100 }}>
+      <div style={{ margin: 100}}>
         <form onSubmit={formSubmit}>
-          <h1>Hello {full_name}</h1>
-          <input
+          <p style={{ textAlign:"center"}}><h1>My Name Is {name.fname} {name.lname}</h1>
+          <h4>Email {name.email}</h4></p><br/><br/><br/>
+          <p>First Name :</p><input
             type="text"
-            style={{ color: "blue", padding: 10 }}
-            placeholder="please enter your name"
+            style={{padding: 10,marginBlock:10}}
+            placeholder="please enter your first name"
             onChange={setValue}
-            value={name}
+            value={name.fname}
+            name="fname"
           />
+          Last Name : <input
+            type="text"
+            style={{padding: 10,marginBlock:10 }}
+            placeholder="please enter your last name"
+            onChange={setValue}
+            value={name.lname}
+            name="lname"
+          />
+          Email : <input
+            type="email"
+            style={{padding: 10 ,marginBlock:10}}
+            placeholder="please enter your email"
+            onChange={setValue}
+            value={name.email}
+            name="email"
+          />
+
           <button
             className="button1"
             type="submit"
